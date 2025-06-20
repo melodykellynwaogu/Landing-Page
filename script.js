c06597800931d618d6e352b8999014d31f1567b4
melodykellynwaogu <melodykellynwaogu224@gmail.com>
2025-06-20 12:56:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function greetUser() {
+	alert("Welcome to M & G Superstore. Please feel free to contact us regarding any product you are interested in.");
+}
+function showThankYouMessage() {
+	document.getElementById("thank-you-message").style.display = "block";
+}
